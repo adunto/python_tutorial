3b2966dacff64ad8808c7872777a24ca4c7874cd
Kwon Soonmin <sogno6037@gmail.com>
2025-04-07 15:01:49 +0900

    1. 변수와 숫자 이해하기 1번 문제

---
+++ Topic1/circle_area.py
@@ -0,0 +1,16 @@
+#  1) 질문: 반지름 5인 원의 넓이를 계산하세요. (공식: 넓이 = π × 반지름², π=3.14) 
+#    파일명: circle_area.py
+
+import math
+
+try:
+    radius = input("반지름을 입력하세요: ")
+    radius = float(radius)
+    if radius < 0:
+        raise ValueError("반지름은 음수가 될 수 없습니다.")
+    
+    area = math.pi * (radius ** 2)
+    print("원의 넓이 : %.1f" % area)
+except ValueError as e:
+    print("잘못된 입력입니다:", e)
+
