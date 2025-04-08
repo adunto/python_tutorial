6db2fe792b619d6733d7cf430d3048bc28739fc5
Kwon Soonmin <sogno6037@gmail.com>
2025-04-08 15:53:17 +0900

    1. 변수와 숫자 이해하기 3번 문제

---
+++ Topic1/time_converter.py
@@ -0,0 +1,15 @@
+# 3) 질문: 12345초를 시간, 분, 초로 변환하세요. 
+#    (공식: 시 = 총초 //3600, 나머지초 = 총초 % 3600, 분 = 나머지초 // 60,  초 = 나머지초 % 60)
+#    파일명: time_converter.py
+
+total_time = 12345
+
+def convert_time(total_time):
+    hours = total_time // 3600
+    remain_time = total_time % 3600
+    minutes = remain_time // 60
+    seconds = remain_time % 60
+    return hours, minutes, seconds
+
+hours, minutes, seconds = convert_time(total_time)
+print(f"총 {total_time}초는 {hours}시간 {minutes}분 {seconds}초입니다.")
